e9d4cc6a3662a4b1e253c56780d1e65688c8dd36
ghSunural <ySunural@yandex.ru>
2019-06-16 23:59:29 +0500

    внесены комментарии

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 //FRONT CONTROLLER
 //namespace Application\Controllers;
-
+ 
+ 
+ //изменения
 use Application as A;
 require_once "config/app_config.php";
 require_once SITE_ROOT."core/autoload.php";
